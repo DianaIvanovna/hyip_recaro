60a6531555b07626676a0de37b8bb44cd81ef2a5
DianaIvanovna <GDI.1999@yandex.ru>
2021-04-29 23:05:41 +0300

    feat: add font roboto

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: `./css/${filename('css')}`
+      filename: `./${filename('css')}`
     }),
   ],
   module: {
