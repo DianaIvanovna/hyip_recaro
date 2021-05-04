9d1c6b16c9d03afaf03223b7cc00c52415a48aed
DianaIvanovna <GDI.1999@yandex.ru>
2021-05-04 21:57:41 +0300

    feat: add smooth scrolling

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 import "./style.scss";
 
+(function(){
+  document.querySelector('.tableOfContents_list').addEventListener("click", (event)=>{
+    event.preventDefault();
+    console.log(event.target.tagName == "A");
+    if (event.target.tagName == "A" && event.target.getAttribute('href')!='#'){
+      console.log(event.target.getAttribute('href'))
+      smoothScroll(event.target.getAttribute('href'));
+    }
+  })
+
+  function smoothScroll(link) {
+    const element = document.querySelector(link);
+    console.log(element);
+    if (element !== null) {
+      let rect = element.getBoundingClientRect();
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      let elementTop = rect.top + scrollTop;
+      window.scroll({
+        left:0,
+        top: elementTop,
+        behavior: 'smooth'
+      })
+    }
+  }
+
+})();
